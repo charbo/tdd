54115bc84a37d88989e3028046aab8a11b1c5d62
Gaël Charbonnier <gael.charbonnier.external@wordline.com>
2019-03-07 13:19:30 +0100

    Franc-ly Speaking

---
+++ src/main/java/org/gch/currency/Franc.java
@@ -0,0 +1,19 @@
+package org.gch.currency;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return this.amount == franc.amount;
+    }
+}
--- src/test/java/org/gch/currency/TestCurrency.java
+++ src/test/java/org/gch/currency/TestCurrency.java
@@ -14,6 +14,14 @@ public class TestCurrency {
         assertEquals(new Dollar(15), five.times(3));
 
     }
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+
+    }
 
     @Test
     public void testEquality() {
